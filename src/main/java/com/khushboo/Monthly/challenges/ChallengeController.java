4b8bf282d55394575115f9636a21819bebf50368
Khushboo0511 <khushboochaudhary0511@gmail.com>
2024-02-05 18:03:31 +0530

    Creating a Challenges REST API with Spring Boot

--- src/main/java/com/khushboo/Monthly/challenges/ChallengeController.java
+++ src/main/java/com/khushboo/Monthly/challenges/ChallengeController.java
@@ -1,5 +1,7 @@
 package com.khushboo.Monthly.challenges;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -7,20 +9,31 @@ import java.util.List;
 
 @RestController
 public class ChallengeController {
-    private List<Challenge> challenges = new ArrayList<>();
 
-    public ChallengeController() {
-        Challenge challenge1 = new Challenge(1L, "January", "Learn a new programming language");
-        challenges.add(challenge1);
+    private ChallengeService challengeService;
+    public ChallengeController(ChallengeService challengeService) {
+        this.challengeService = challengeService;
     }
     @GetMapping("/challenges")
     public List<Challenge> getAllChallenges() {
-        return challenges;
+        return challengeService.getAllChallenges();
     }
 
     @PostMapping("/challenges")
-    public String addChallenges(@RequestBody Challenge challenge) {
-        challenges.add(challenge);
-        return "Challenge added successfuly";
+    public String addChallenge(@RequestBody Challenge challenge) {
+        challengeService.addChallenge(challenge);
+        return "Challenge added successfully";
+    }
+
+    @GetMapping("/challenges/{month}")
+    public ResponseEntity<Challenge> getChallenge(@PathVariable String month) {
+
+        Challenge challenge = challengeService.getChallenge(month);
+
+        if (challenge!=null) {
+            return new ResponseEntity<>(challenge, HttpStatus.OK) ;
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
--- src/main/java/com/khushboo/Monthly/challenges/ChallengeService.java
+++ src/main/java/com/khushboo/Monthly/challenges/ChallengeService.java
@@ -1,5 +1,49 @@
 package com.khushboo.Monthly.challenges;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class ChallengeService {
 
+    private static List<Challenge> challenges = new ArrayList<>();
+    private Long nextId = 1L;
+
+    public ChallengeService() {
+    }
+
+    public static List<Challenge> getAllChallenges() {
+        return challenges;
+    }
+
+    public boolean addChallenge(Challenge challenge) {
+        if (challenge!=null) {
+            challenge.setId(nextId++);
+            challenges.add(challenge);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @PostMapping("/challenges")
+    public String addChallenges(@RequestBody Challenge challenge) {
+        challenges.add(challenge);
+        return "Challenge added successfuly";
+    }
+
+    public Challenge getChallenge(String month) {
+        for (Challenge challenge: challenges) {
+            if (challenge.getMonth().equalsIgnoreCase(month)) {
+                return challenge;
+            }
+        }
+        return null;
+    }
 }
